ff5c398b9ae3d8dcb4d4c7cac0aa71414d11ab65
chehr <chehrazad.ouazzani@student.vinci.be>
2023-02-06 08:36:39 +0100

    first commit

---
+++ src/main/java/be/vinci/api/FilmResource.java
@@ -0,0 +1,86 @@
+package be.vinci.api;
+
+import be.vinci.Film;
+import be.vinci.services.FilmDataService;
+import be.vinci.services.Json;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.apache.commons.text.StringEscapeUtils;
+
+import java.util.List;
+
+@Singleton
+@Path("films")
+public class FilmResource {
+
+private FilmDataService myFilmDataService = new FilmDataService();
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public List<Film> getAll(
+      @DefaultValue("-1") @QueryParam("minimum-duration") int minimumDuration) {
+      return myFilmDataService.getAll(minimumDuration);
+  }
+
+
+  @GET
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Film getOne(@PathParam("id") int id) {
+    Film filmFound =  myFilmDataService.getOne(id);
+    if (filmFound == null){
+      throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+              .entity("Ressource not found").type("text/plain").build());
+    }
+    return filmFound;
+  }
+
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Film createOne(Film film) {
+      if (film == null || film.getTitle() == null || film.getTitle().isBlank()) {
+          throw new WebApplicationException(
+              Response.status(Response.Status.BAD_REQUEST).entity("Lacks of mandatory info")
+                  .type("text/plain").build());
+      }
+    return myFilmDataService.createOne(film);
+  }
+
+  @DELETE
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Film deleteOne(@PathParam("id") int id) {
+      if (id == 0) // default value of an integer => has not been initialized
+      {
+          throw new WebApplicationException(
+              Response.status(Response.Status.BAD_REQUEST).entity("Lacks of mandatory id info")
+                  .type("text/plain").build());
+      }
+      Film filmToDelete =  myFilmDataService.deleteOne(id);
+    if (filmToDelete == null) {
+          throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+              .entity("Ressource not found").type("text/plain").build());
+      }
+    return filmToDelete;
+  }
+
+  @PUT
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Film updateOne(Film film, @PathParam("id") int id) {
+      if (id == 0 || film == null || film.getTitle() == null || film.getTitle().isBlank()) {
+          throw new WebApplicationException(
+              Response.status(Response.Status.BAD_REQUEST).entity("Lacks of mandatory info")
+                  .type("text/plain").build());
+      }
+      Film filmToUpdate = myFilmDataService.updateOne(film,id);
+      if (filmToUpdate == null) {
+          throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+              .entity("Ressource not found").type("text/plain").build());
+      }
+    return filmToUpdate;
+  }
+}
